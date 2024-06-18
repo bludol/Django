c755c9d8db24b194c9dc2d7686ec00b66cedc72a
bludol <61668069+bludol@users.noreply.github.com>
2024-06-18 08:30:53 +0200

    Meta models.py
    Přidání meta dat a verbose name do souboru models.py

--- RFPproject/RFPapp/models.py
+++ RFPproject/RFPapp/models.py
@@ -1,29 +1,44 @@
 from django.db import models
 
 class Kapela(models.Model):
-    jmeno = models.CharField(max_length=45, unique=True)
-    zanr = models.CharField(max_length=45)
-    obrazek = models.ImageField(upload_to='img/kapely', blank=True, null=True)
-    popis = models.TextField(blank=True)
+    jmeno = models.CharField(max_length=45, unique=True, verbose_name='Jméno kapely')
+    zanr = models.CharField(max_length=45, verbose_name='Žánr kapely')
+    obrazek = models.ImageField(upload_to='img/kapely', blank=True, null=True, verbose_name='Fotka kapely')
+    popis = models.TextField(blank=True, verbose_name='Popis kapely')
+
+    class Meta:
+        ordering = ['jmeno']
+        verbose_name = 'Kapela'
+        verbose_name_plural = 'Kapely'
 
     def __str__(self):
         return f"{self.jmeno} - {self.zanr}"
 
 class Osoba(models.Model):
-    jmeno = models.CharField(max_length=45)
-    prijmeni = models.CharField(max_length=45)
+    jmeno = models.CharField(max_length=45, verbose_name='Jméno osoby')
+    prijmeni = models.CharField(max_length=45, verbose_name='Příjmení osoby')
     kapela_jmeno = models.ForeignKey(Kapela, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['prijmeni', 'jmeno']
+        verbose_name = 'Osoba'
+        verbose_name_plural = 'Osoby'
+
+    def seznam_osob(self):
+        return Osoba.objects.filter(kapela_jmeno=self)
 
     def __str__(self):
         return f"{self.jmeno} {self.prijmeni}"
 
 class Pisnicka(models.Model):
-    jmeno_pisne = models.CharField(max_length=45)
-    zanr = models.CharField(max_length=45)
+    jmeno_pisne = models.CharField(max_length=45, verbose_name='Jméno písně')
+    zanr = models.CharField(max_length=45, verbose_name='Žánr písně')
     kapela_jmeno = models.ForeignKey(Kapela, on_delete=models.CASCADE)
 
-    def seznam_osob(self):
-        return Osoba.objects.filter(kapela_jmeno=self)
+    class Meta:
+        ordering = ['jmeno_pisne']
+        verbose_name = 'Píseň'
+        verbose_name_plural = 'Písně'
 
     def seznam_pisnicek(self):
         return Pisnicka.objects.filter(kapela_jmeno=self)
